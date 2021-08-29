1fd5d37832e902ea7630e9af00498d2c33cf3ed0
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-29 07:53:32 -0500

    monthlyTotal added to submitToTableFunction

--- client.js
+++ client.js
@@ -1,5 +1,4 @@
 console.log('JS Ready');
-let monthlyTotal = 0;
 
 $(document).ready(readyNow);
 
@@ -7,12 +6,16 @@ function readyNow (){
     console.log('JQ Ready');
     $('#submit-button').on('click', submitToTable)
 }
+
+let monthlyTotal = 0;
+
 // define inputs as variables
 // append values from inputs
 // call clearInputs function
 
 function submitToTable () {
     console.log('in submitToTable');
+
     let firstName = $('#first-name-input');
     let lastName = $('#last-name-input');
     let id = $('#id-input');
@@ -29,8 +32,15 @@ function submitToTable () {
             <td class="delete-table-body"><button class="delete-button">Delete</button></td>
         </tr>
     `);
+
+    monthlyTotal += parseInt(annualSalary.val());
+    console.log(monthlyTotal);
+    // Cannot read commas when concatenated, how would I fix this?
+
+    $('#total-monthly-span').empty();
+    $('#total-monthly-span').append((monthlyTotal));
+
     clearInputs();
-    addToMonthlyTotal();
 }
 
 function clearInputs () {
@@ -41,13 +51,19 @@ function clearInputs () {
     $('#annual-salary-input').val('');
 }
 
-function addToMonthlyTotal () {
-    console.log('in addToMonthlyTotal');
-    let employeeSalary = $('#annual-salary-input').val();
+// #annual-salary-input is not read from 
+// addToMonthlyTotal function, it is read as
+// 0. I think it is not being passed correctly.
+// When defined globally it is also unable to be read.
+// 
 
-    monthlyTotal += employeeSalary;
-    console.log(monthlyTotal);
+// function addToMonthlyTotal () {
+//     console.log('in addToMonthlyTotal');
+//     let employeeSalary = $('#annual-salary-input');
+
+//     monthlyTotal += employeeSalary.val();
+//     console.log(monthlyTotal);
     
-    $('#total-monthly-span').empty();
-    $('#total-monthly-span').append(parseInt(monthlyTotal));
-}
+//     $('#total-monthly-span').empty();
+//     $('#total-monthly-span').append(parseInt(monthlyTotal));
+// }
